0c4229674d2a6bd33414e99bd7a5832af96cfd3d
Wei Rong <wei.rong@crypto.com>
2023-05-16 02:35:59 +0000

    update sample

--- sample/build.sh
+++ sample/build.sh
@@ -2,8 +2,11 @@
 
 echo "build and pack node-vsock..."
 cd ..
-yarn build:all
+yarn build:debug --target=x86_64-unknown-linux-musl
+yarn build:debug --target=x86_64-unknown-linux-gnu
+yarn build:ts
 yarn pack
+
 mkdir -p sample/tmp/
 mv -f *.tgz sample/tmp/node-vsock.tgz
 cp -f *.node sample/tmp/
@@ -15,10 +18,11 @@ fi
 echo "build sample..."
 cd "sample"
 yarn install --ignore-scripts && yarn build
+cp -f tmp/node-vsock.linux-x64-gnu.node node_modules/node-vsock/
 
 if [ "$1" != "all" ]; then
   exit 0
 fi
 
 echo "build sample server docker image..."
-docker build -f ./Dockerfile.server -t node-vsock-sample-server
+docker build -f ./Dockerfile.server -t node-vsock-sample-server .
